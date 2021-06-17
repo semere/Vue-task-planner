518f73cd26a3d52b654e7d04218bdf8168ff0b5c
semere abraham <semeretesfai4@yahoo.com>
2021-06-17 15:04:20 +0200

    Updated

---
+++ src/views/EditProject.vue
@@ -0,0 +1,42 @@
+<template>
+<form @submit.prevent="editSubmit">
+  <label>Title:</label>
+  <input type="text" v-model="title" required>
+  <label>Details:</label>
+  <textarea v-model="details" required></textarea>
+  <button>Edit Project</button>
+  </form>
+</template>
+<script>
+export default {
+  props: ['id'],
+  data() {
+    return {
+      title: '',
+      details: '',
+      url: 'http://localhost:3000/projects/' + this.id
+    }
+  },
+    mounted() {
+        fetch(this.url)
+          .then(res => res.json())
+          .then(data => {
+              this.title = data.title
+              this.details = data.details
+          })
+    },
+    methods: {
+      editSubmit() {
+        fetch(this.url, {
+          method: 'PATCH',
+          headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({ title: this.title, details: this.details })
+          }).then(() => {
+            this.$router.push('/')
+          }).catch((err) => console.log(err))
+      }
+    }
+  }
+  </script>
+  <style>
+  </style>
